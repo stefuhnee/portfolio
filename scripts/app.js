51c4cc3f7f0227739c115553668ab33a3fddeedd
Stefanie Hansen <stefuhnee@gmail.com>
2016-04-11 16:59:12 -0700

    Wrote JS using the model from the pair programming project. Not posting the object info to the template.. need to troubleshoot

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,28 @@
-var
+var projects = [];
+
+function Project (properties) {
+  this.title = properties.title;
+  this.postDate = properties.postDate;
+  this.projectBody = properties.projectBody;
+}
+
+Project.prototype.toHTML = function() {
+  var $newProject = $('article.template').clone();
+  $newProject.find('h1').text(this.title);
+  $newProject.find('projectBody').html(this.projectBody);
+  $newProject.find('time[postDate]').attr('title', this.postDate);
+  $newProject.find('time').html('roughly ' + parseInt((new Date() - new Date(this.postDate))/60/60/24/1000) + ' days ago');
+  $newProject.append('<hr>');
+};
+
+portfolioProjects.sort(function(a,b) {
+  return (new Date(b.postDate)) - (new Date(a.postDate));
+});
+
+portfolioProjects.forEach(function(ele) {
+  projects.push(new Project(ele));
+});
+
+projects.forEach(function(a) {
+  $('#projects').append(a.toHTML());
+});
--- scripts/projects.js
+++ scripts/projects.js
@@ -1,3 +1,7 @@
 var portfolioProjects = [
-
+  {
+    title: 'Portfolio Project (so meta)',
+    postDate: '2016-04-11',
+    projectBody: 'Ipsum stuff'
+  }
 ];
