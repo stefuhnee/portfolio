97ed6f0464ecd7d05a2f2db0966c978bf4eef46a
Stefanie Hansen <stefuhnee@gmail.com>
2016-04-12 16:05:49 -0700

    Added nav bar for extra pages within webpage, added another paragraph to project bodies, added some styling and a font for my name in the header

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,17 +1,17 @@
 var portfolioProjects = [
   {
-    title: 'Portfolio Project (so meta)',
+    title: 'Portfolio Project',
     postDate: '2016-04-10',
-    projectBody: '<p>Ex adhuc movet scriptorem per, quo ad omnes legimus. Te nec magna clita maiorum, ex his utinam aliquam reprimique. No viderer adversarium nec, aperiri volumus no vis, harum oporteat mediocrem his id. Dicam putant vim ea, no affert explicari his, idque philosophia necessitatibus qui ea. Eu vis ignota nemore delicata, solum verterem referrentur ut eum. Nec id vocent audire accommodar</p>'
+    projectBody: '<p>Ex adhuc movet scriptorem per, quo ad omnes legimus. Te nec magna clita maiorum, ex his utinam aliquam reprimique. No viderer adversarium nec, aperiri volumus no vis, harum oporteat mediocrem his id. Dicam putant vim ea, no affert explicari his, idque philosophia necessitatibus qui ea. Eu vis ignota nemore delicata, solum verterem referrentur ut eum. Nec id vocent audire accommodar</p>  <p>Ex adhuc movet scriptorem per, quo ad omnes legimus. Te nec magna clita maiorum, ex his utinam aliquam reprimique. No viderer adversarium nec, aperiri volumus no vis, harum oporteat mediocrem his id. Dicam putant vim ea, no affert explicari his, idque philosophia necessitatibus qui ea. Eu vis ignota nemore delicata, solum verterem referrentur ut eum. Nec id vocent audire accommodar</p>'
   },
   {
-    title: 'Portfolio Project (so meta)',
+    title: 'Portfolio Project',
     postDate: '2016-04-09',
-    projectBody: '<p>Ex adhuc movet scriptorem per, quo ad omnes legimus. Te nec magna clita maiorum, ex his utinam aliquam reprimique. No viderer adversarium nec, aperiri volumus no vis, harum oporteat mediocrem his id. Dicam putant vim ea, no affert explicari his, idque philosophia necessitatibus qui ea. Eu vis ignota nemore delicata, solum verterem referrentur ut eum. Nec id vocent audire accommodar</p>'
+    projectBody: '<p>Ex adhuc movet scriptorem per, quo ad omnes legimus. Te nec magna clita maiorum, ex his utinam aliquam reprimique. No viderer adversarium nec, aperiri volumus no vis, harum oporteat mediocrem his id. Dicam putant vim ea, no affert explicari his, idque philosophia necessitatibus qui ea. Eu vis ignota nemore delicata, solum verterem referrentur ut eum. Nec id vocent audire accommodar</p> <p>Ex adhuc movet scriptorem per, quo ad omnes legimus. Te nec magna clita maiorum, ex his utinam aliquam reprimique. No viderer adversarium nec, aperiri volumus no vis, harum oporteat mediocrem his id. Dicam putant vim ea, no affert explicari his, idque philosophia necessitatibus qui ea. Eu vis ignota nemore delicata, solum verterem referrentur ut eum. Nec id vocent audire accommodar</p>'
   },
   {
-    title: 'Portfolio Project (so meta)',
+    title: 'Portfolio Project',
     postDate: '2016-04-10',
-    projectBody: '<p>Ex adhuc movet scriptorem per, quo ad omnes legimus. Te nec magna clita maiorum, ex his utinam aliquam reprimique. No viderer adversarium nec, aperiri volumus no vis, harum oporteat mediocrem his id. Dicam putant vim ea, no affert explicari his, idque philosophia necessitatibus qui ea. Eu vis ignota nemore delicata, solum verterem referrentur ut eum. Nec id vocent audire accommodar</p>'
+    projectBody: '<p>Ex adhuc movet scriptorem per, quo ad omnes legimus. Te nec magna clita maiorum, ex his utinam aliquam reprimique. No viderer adversarium nec, aperiri volumus no vis, harum oporteat mediocrem his id. Dicam putant vim ea, no affert explicari his, idque philosophia necessitatibus qui ea. Eu vis ignota nemore delicata, solum verterem referrentur ut eum. Nec id vocent audire accommodar</p> <p>Ex adhuc movet scriptorem per, quo ad omnes legimus. Te nec magna clita maiorum, ex his utinam aliquam reprimique. No viderer adversarium nec, aperiri volumus no vis, harum oporteat mediocrem his id. Dicam putant vim ea, no affert explicari his, idque philosophia necessitatibus qui ea. Eu vis ignota nemore delicata, solum verterem referrentur ut eum. Nec id vocent audire accommodar</p>'
   }
 ];
