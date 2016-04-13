b607d16e6d6dc3451afc7e653a5dedec1583380b
Stefanie Hansen <stefuhnee@gmail.com>
2016-04-13 15:45:33 -0700

    added one real project to my projects array, added href to constructor and project creating method. added image of first project. working on styling

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,20 +1,23 @@
 var portfolioProjects = [
   {
-    title: 'Portfolio Project',
-    postDate: '2016-04-10',
-    category: 'front end',
-    projectBody: '<p>Ex adhuc movet scriptorem per, quo ad omnes legimus. Te nec magna clita maiorum, ex his utinam aliquam reprimique. No viderer adversarium nec, aperiri volumus no vis, harum oporteat mediocrem his id. Dicam putant vim ea, no affert explicari his, idque philosophia necessitatibus qui ea. Eu vis ignota nemore delicata, solum verterem referrentur ut eum. Nec id vocent audire accommodar</p>  <p>Ex adhuc movet scriptorem per, quo ad omnes legimus. Te nec magna clita maiorum, ex his utinam aliquam reprimique. No viderer adversarium nec, aperiri volumus no vis, harum oporteat mediocrem his id. Dicam putant vim ea, no affert explicari his, idque philosophia necessitatibus qui ea. Eu vis ignota nemore delicata, solum verterem referrentur ut eum. Nec id vocent audire accommodar</p>'
+    title: 'Pizza 3000',
+    postDate: '2016-03-23',
+    category: 'lab',
+    location: 'http://stefuhnee.github.io/pizza-odyssey/',
+    projectBody: '<img src="img/pizza.png" /> <p>I created this website during the second week of Code 201 at Code Fellows. Most of the main functionality was implemented in <a href="http://stefuhnee.github.io/pizza-odyssey/sales-data.html">another page</a> which allows the user to enter location-specific sales data for a fictional pizza store.</p>'
   },
   {
     title: 'Portfolio Project',
     postDate: '2016-04-09',
     category: 'back end',
+    location: '',
     projectBody: '<p>Ex adhuc movet scriptorem per, quo ad omnes legimus. Te nec magna clita maiorum, ex his utinam aliquam reprimique. No viderer adversarium nec, aperiri volumus no vis, harum oporteat mediocrem his id. Dicam putant vim ea, no affert explicari his, idque philosophia necessitatibus qui ea. Eu vis ignota nemore delicata, solum verterem referrentur ut eum. Nec id vocent audire accommodar</p> <p>Ex adhuc movet scriptorem per, quo ad omnes legimus. Te nec magna clita maiorum, ex his utinam aliquam reprimique. No viderer adversarium nec, aperiri volumus no vis, harum oporteat mediocrem his id. Dicam putant vim ea, no affert explicari his, idque philosophia necessitatibus qui ea. Eu vis ignota nemore delicata, solum verterem referrentur ut eum. Nec id vocent audire accommodar</p>'
   },
   {
     title: 'Portfolio Project',
     postDate: '2016-04-10',
     category: 'fiddle',
+    location: '',
     projectBody: '<p>Ex adhuc movet scriptorem per, quo ad omnes legimus. Te nec magna clita maiorum, ex his utinam aliquam reprimique. No viderer adversarium nec, aperiri volumus no vis, harum oporteat mediocrem his id. Dicam putant vim ea, no affert explicari his, idque philosophia necessitatibus qui ea. Eu vis ignota nemore delicata, solum verterem referrentur ut eum. Nec id vocent audire accommodar</p> <p>Ex adhuc movet scriptorem per, quo ad omnes legimus. Te nec magna clita maiorum, ex his utinam aliquam reprimique. No viderer adversarium nec, aperiri volumus no vis, harum oporteat mediocrem his id. Dicam putant vim ea, no affert explicari his, idque philosophia necessitatibus qui ea. Eu vis ignota nemore delicata, solum verterem referrentur ut eum. Nec id vocent audire accommodar</p>'
   }
 ];
