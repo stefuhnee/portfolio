dd1698d5d1eb68a8c4a547a0c6c762847e979f81
Stefanie Hansen <stefuhnee@gmail.com>
2016-04-12 20:22:15 -0700

    added show less link within html and added logic to make the show less click event work

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -13,9 +13,17 @@ projectView.handleMainNav = function() {
 projectView.setTeasers = function() {
   var $extraParagraphs = $('.project-body *:nth-of-type(n+2)');
   $extraParagraphs.hide();
+  $('a.read-less').hide();
   $('a.read-on').on('click', function(e) {
     e.preventDefault();
     $(this).prev().children().fadeIn();
+    $(this).hide();
+    $(this).next().show();
+  });
+  $('a.read-less').on('click', function(e) {
+    e.preventDefault();
+    $extraParagraphs.fadeOut();
+    $(this).prev().show();
     $(this).hide();
   });
 };
