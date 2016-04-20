89bfd2239684e99db5ff5da686d2cbd2b472fc77
Stefanie Hansen <stefuhnee@gmail.com>
2016-04-19 18:35:26 -0700

    Added IFFEs, mapped the Article array, mapped the data file to produce my Project array, revamped my ajax calls to be more succinct, added a getAll function to DRY out my code, made sure that everything is accessible that needs to be within the IFFE.

--- scripts/app.js
+++ scripts/app.js
@@ -1,64 +1,58 @@
-var categories = [];
+(function(module){
 
-function Project (projects) {
-  for (key in projects) {
-    this[key] = projects[key];
+  var categories = [];
+
+  function Project (projects) {
+    for (key in projects) {
+      this[key] = projects[key];
+    };
   };
-};
 
-Project.all = [];
+  Project.prototype.toHTML = function(template) {
+    var template = Handlebars.compile($(template).html());
 
-Project.prototype.toHTML = function(template) {
-  var template = Handlebars.compile($(template).html());
+    this.daysAgo = parseInt((new Date() - new Date(this.postDate))/60/60/24/1000);
+    this.publishStatus = this.postDate ? 'Posted ' + this.daysAgo + ' days ago' : '(draft)';
+    return template(this);
+  };
 
-  this.daysAgo = parseInt((new Date() - new Date(this.postDate))/60/60/24/1000);
-  this.publishStatus = this.postDate ? 'Posted ' + this.daysAgo + ' days ago' : '(draft)';
-  return template(this);
-};
+  Project.loadAll = function(data) {
+    data.sort(function(a,b) {
+      return (new Date(b.postDate)) - (new Date(a.postDate));
+    });
+    Project.all = data.map(function(ele) {
+      return new Project(ele);
+    });
+  };
 
-Project.loadAll = function(data) {
-  data.sort(function(a,b) {
-    return (new Date(b.postDate)) - (new Date(a.postDate));
-  });
-  data.forEach(function(ele) {
-    Project.all.push(new Project(ele));
-  });
-};
-
-Project.fetchAll = function() {
-  localStorage.eTag = JSON.stringify(0);
-  if (localStorage.projects) {
-    $.ajax( {
-      type: 'HEAD',
-      url: 'data/projects.json',
-      success: function(data, message, xhr) {
-        var eTag = xhr.getResponseHeader('eTag');
-        if (eTag !== localStorage.eTag) {
-          $.getJSON('data/projects.json', function(data) {
-            Project.loadAll(data);
-            localStorage.projects = JSON.stringify(Project.all);
+  Project.fetchAll = function(page) {
+    if (localStorage.projects) {
+      $.ajax( {
+        type: 'HEAD',
+        url: 'data/projects.json',
+        success: function(data, message, xhr) {
+          var eTag = xhr.getResponseHeader('eTag');
+          if (!localStorage.eTag || eTag !== localStorage.eTag) {
             localStorage.eTag = eTag;
-            projectView.initIndexPage();
-          });
-        } else {
-          Project.loadAll(JSON.parse(localStorage.projects));
-          projectView.initIndexPage();
+            Project.getAll();
+          } else {
+            Project.loadAll(JSON.parse(localStorage.projects));
+            page();
+          }
         }
-      }
-    });
-  } else {
-    $.ajax( {
-      type: 'HEAD',
-      url: 'data/projects.json',
-      success: function(data, message, xhr) {
-        var eTag = xhr.getResponseHeader('eTag');
-        localStorage.eTag = eTag;
-      }
-    });
+      });
+    } else {
+      Project.getAll();
+    }
+  };
+
+  Project.getAll = function() {
     $.getJSON('data/projects.json', function(data){
       Project.loadAll(data);
       localStorage.projects = JSON.stringify(Project.all);
       projectView.initIndexPage();
     });
-  }
-};
+  };
+
+  module.Project = Project;
+})(window);
