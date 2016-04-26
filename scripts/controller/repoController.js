bd2137512d2bb520c30488d9ef0ca77ad4c29dde
Stefanie Hansen <stefuhnee@gmail.com>
2016-04-26 16:01:58 -0700

    fixed githubToken, now correctly accessing API

--- scripts/controller/repoController.js
+++ scripts/controller/repoController.js
@@ -3,6 +3,8 @@
 
   repoController.index = function() {
     $('.repo').show().siblings().hide();
+    $('.filters').hide();
+    $('#main-header h1').hide();
     repos.requestRepos(repoView.index);
   };
 
